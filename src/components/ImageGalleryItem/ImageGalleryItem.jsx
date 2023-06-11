8841203e45df4933451beba5ec04b38223f0d645
Andrii Triukhan <39101979+em0nti@users.noreply.github.com>
2023-06-11 23:07:49 +0300

    Makes some small changes

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -2,18 +2,32 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './ImageGalleryItem.module.css';
 
-const ImageGalleryItem = ({ webformatURL, largeImageURL, tags }) => {
+const ImageGalleryItem = ({
+  webformatURL,
+  webformatWidth,
+  webformatHeight,
+  largeImageURL,
+  tags,
+}) => {
   return (
     <li className={styles.galleryItem}>
-      <img className={styles.galleryItemImage} src={webformatURL} alt={tags} />
+      <img
+        className={styles.galleryItemImage}
+        src={webformatURL}
+        alt={tags}
+        width={webformatWidth}
+        height={webformatHeight}
+      />
     </li>
   );
 };
 
 ImageGalleryItem.propTypes = {
   webformatURL: PropTypes.string.isRequired,
-  largeImageURL:PropTypes.string,
-  tags: PropTypes.string
+  webformatWidth: PropTypes.number,
+  webformatHeight: PropTypes.number,
+  largeImageURL: PropTypes.string,
+  tags: PropTypes.string,
 };
 
 export default ImageGalleryItem;
